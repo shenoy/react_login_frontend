ea6fed6fd92227cf6c6bdd33af5ea14f6d1bd329
Rajesh Shenoy <rajeshtshenoy@gmail.com>
2020-05-12 11:15:03 +0100

    make it betta

--- src/Photos.js
+++ src/Photos.js
@@ -2,7 +2,14 @@ import React from "react";
 
 class Photos extends React.Component {
   render() {
-    return <div className="photosPanel"></div>;
+    return (
+      <div className="photosPanel">
+        <div className="photos-title">Photos</div>
+        <input className="file-input" type="file" />
+        <button>Upload</button>
+        <div>Photos</div>
+      </div>
+    );
   }
 }
 
